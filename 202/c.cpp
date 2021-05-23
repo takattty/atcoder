27f042a297b2291082ea025dc01f52683575920e
takattty <r9r_celtics_t9t@icloud.com>
2021-05-23 14:12:52 +0900

    refactaring c

--- 202/c.cpp
+++ 202/c.cpp
@@ -1,103 +1,89 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main() {
-  int n, result, hey;
-  cin >> n;
+// int main() {
+//   int n, result, hey;
+//   cin >> n;
 
-  vector<int> a(n);
-  vector<int> b(n);
-  vector<int> c(n);
-  vector<int> d(n);
+//   vector<int> a(n);
+//   vector<int> b(n);
+//   vector<int> c(n);
+//   vector<int> d(n);
 
-  for(int i=0; i<n; i++) {
-    cin >> a.at(i);
-  }
+//   for(int i=0; i<n; i++) {
+//     cin >> a.at(i);
+//   }
 
-  for(int i=0; i<n; i++) {
-    cin >> b.at(i);
-  }
+//   for(int i=0; i<n; i++) {
+//     cin >> b.at(i);
+//   }
 
-  for(int i=0; i<n; i++) {
-    cin >> c.at(i);
-  }
+//   for(int i=0; i<n; i++) {
+//     cin >> c.at(i);
+//   }
 
-  // for(int i=0; i<n; i++) {
-  //   for(int j=0; j<n; j++) {
-  //     if(b.at((c.at(i))-1) == a.at(j)) {
-  //       res++;
-  //     }
-  //   }
-  // }
+//   // for(int i=0; i<n; i++) {
+//   //   for(int j=0; j<n; j++) {
+//   //     if(b.at((c.at(i))-1) == a.at(j)) {
+//   //       res++;
+//   //     }
+//   //   }
+//   // }
 
-  for(int i=0; i<n; i++) {
-    d.at(i) = b.at((c.at(i))-1);
-  }
+//   for(int i=0; i<n; i++) {
+//     d.at(i) = b.at((c.at(i))-1);
+//   }
 
-  for(int i=0; i<n; i++) {
-    cout << a.at(i);
-    cout << d.at(i);
-    auto j = find(d.begin(), d.end(), a.at(i));
-    if(j != d.end()) {
-      result++;
-    } else {
-      hey++;
-    }
-  }
+//   for(int i=0; i<n; i++) {
+//     cout << a.at(i);
+//     cout << d.at(i);
+//     auto j = find(d.begin(), d.end(), a.at(i));
+//     if(j != d.end()) {
+//       result++;
+//     } else {
+//       hey++;
+//     }
+//   }
 
 
 
-  cout << endl;
-  cout << result << endl;
-  cout << endl;
-  cout << hey << endl;
-}
+//   cout << endl;
+//   cout << result << endl;
+//   cout << endl;
+//   cout << hey << endl;
+// }
 
 // 参考コード
-// #include <bits/stdc++.h>
-// #include <iostream>
-// using namespace std;
-// void solve(){
-//     int n;
-//     cin>>n;
-//     int a[n];
-//     int b[n];
-//     int c[n];
-//     for(int i=0;i<n;i++){
-//         cin>>a[i];
-//     }
-//       for(int i=0;i<n;i++){
-//         cin>>b[i];
-//     }
-//       for(int i=0;i<n;i++){
-//         cin>>c[i];
-//     }
-//     int ct[n+1];
-//     int ct1[n+1];
-//     for(int j=0;j<=n;j++){
-//         ct[j]=0;
-//         ct1[j]=0;
-//     }
-//     for(int i=0;i<n;i++){
-//         ct[c[i]]++;
-//     }
-//     for(int i=0;i<n;i++){
-//         ct1[a[i]]++;
-//     }
-//     long long ans=0;
-//     for(int i=0;i<n;i++){
-//         if(ct1[b[i]]>0){
-//             ans=ans+ct1[b[i]]*ct[i+1];
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
-//         }
-//     }
-   
-//     cout<<ans<<endl;
-// }
+int main(){
+  int n;
+  cin>>n;
+  vector<int> a(n), b(n), c(n);
+  vector<int> ct(n+1); // 要素はnなんだけど、1≤N≤10で今は考えているから。でも正直腑に落ちてはいないから、再度復習しようね。
 
-// int main(){
 
-//     solve();
+  for(int i=0;i<n;i++){
+      cin>>a.at(i);
+  }
+    for(int i=0;i<n;i++){
+      cin>>b.at(i);
+  }
+    for(int i=0;i<n;i++){
+      cin>>c.at(i);
+  }
+  
+  // ここはBcjの値の数を新しい配列に代入。ここは値ではなくて、値の数を代入する事が一番大事な考え方。2重ループせずに計算量を抑えられるから。
+  for(int i=0; i<n; i++) {
+    ct.at(b.at(c.at(i)-1))++;
+  }
 
-//     return 0;
-// }
+  // ctの要素はBcjの値の数なので、Aiの値がctの添字と一致し取得した値を足し合わせていく。
+  long long ans=0;
+  for(int i=0; i<n; i++) {
+    ans += ct.at(a.at(i));
+  }
+  cout<<ans<<endl;
+}
