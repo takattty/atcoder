bf212d1f6aacf21fa6c048a57c0f2e60fc37cd28
takattty <r9r_celtics_t9t@icloud.com>
2021-05-23 12:32:21 +0900

    refactaring b

--- 202/b.cpp
+++ 202/b.cpp
@@ -18,3 +18,18 @@ int main() {
 
   cout << n << endl;
 }
+
+// これは公式と考え方と流れは一緒でしたので、省略。強いていうならもう少し短く書けますよくらいかな。
+// int main() {
+//   string n;
+//   cin >> n;
+
+//   reverse(n.begin(), n.end());
+
+//   for(int i = 0; i < n.size(); i++) {
+//     if(n[i] == '6') n[i] = '9';
+//     else if(n[i] == '9') n[i] = '6';
+//   }
+
+//   cout << n << endl;
+// }
