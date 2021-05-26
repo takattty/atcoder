f232c3009f51725141ecb2e114b179e58c51e1ea
takattty <r9r_celtics_t9t@icloud.com>
2021-05-27 00:15:45 +0900

    done 197-a

---
+++ 197/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  // char cs = s[0];
+  // s.erase(0, 1);
+  // cout << s + cs << endl;
+  cout << s[1] << s[2] << s[0] << endl;
+}
+
+// 公式の見て絶望してしまった。めちゃくちゃ簡単じゃん。
