dc95c67fa79fffba335a43642d560e7ee17b4b75
takattty <r9r_celtics_t9t@icloud.com>
2021-05-29 23:44:59 +0900

    wip 199

---
+++ 199/b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  vector<int> a(n), b(n), c(1000);
+
+  for(int i=0; i<n; i++) {
+    cin >> a.at(i);
+  }
+  for(int i=0; i<n; i++) {
+    cin >> b.at(i);
+  }
+
+
+  for(int i=0; i<n; i++) {
+
+  }
+}
