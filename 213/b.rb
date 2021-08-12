5f5fdedb0b681eb04658d76750d7b321a1efd947
takattty <r9r_celtics_t9t@icloud.com>
2021-08-13 00:00:43 +0900

    213 b used by ruby

---
+++ 213/b.rb
@@ -0,0 +1,13 @@
+num = gets.to_i
+values = gets.split(' ').map(&:to_i)
+value_hash = Hash.new([])
+
+i = 0
+num.times {
+  value_hash[i] = values[i]
+  i = i + 1
+}
+
+new_value_hash = value_hash.sort_by { |key, value| value }
+player = new_value_hash[num-2][0]
+p player+1
