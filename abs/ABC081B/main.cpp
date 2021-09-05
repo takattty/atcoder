1840d06388bbf6815bb69c223701a7762cf5972a
takattty <r9r_celtics_t9t@icloud.com>
2021-09-05 13:08:39 +0900

    done 081-b

--- abs/ABC081B/main.cpp
+++ abs/ABC081B/main.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 bool even = true;
 
-bool isEven(vector<int> x) {
-    for(int el : x) if(el%2 != 0) even = false;
+bool isEven(vector<int> a) {
+    for(int x : a) {
+        if(x%2 != 0) even = false;
+    }
     return even;
 }
 
 int main() {
     int n, result=0;
-    vector<int> a(n);
     cin >> n;
+    vector<int> a(n);
 
     for(int i=0; i<n; i++) {
         cin >> a.at(i);
@@ -20,6 +22,7 @@ int main() {
         for(int i=0; i<n; i++) {
             a.at(i) = a.at(i) / 2;
         }
+        result++;
     }
 
     cout << result << endl;
