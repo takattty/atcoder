5e0b3ab4340aa789cf84f50950d2221efdc2a8c1
takattty <r9r_celtics_t9t@icloud.com>
2021-10-11 22:44:36 +0900

    wip 222-c

--- abc222/C/main.cpp
+++ abc222/C/main.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     int n, m; cin >> n >> m;
-    vector< vector<int> > data(n, vector<int>(m));
-    map<int, int> result;
+    vector< vector<char> > data(2*n, vector<char>(m));
+    vector<pair<int, int> > result(2*n);
 
     for(int i=0; i<2*n; i++) {
-        result[i] = 0;
+        result.at(i) = make_pair(0, i+1);
     }
 
     for(int i=0; i<2*n; i++) {
@@ -16,25 +16,36 @@ int main() {
         }
     }
 
-    // for(int j=0; j<m; j++) {
-    //     for(int k=1; k<=n; k++) {
-    //        if(data.at(2*k-1).at(j) == 'G' && data.at(2*k).at(j) == 'P') {
-    //             result[2*k-1]++;
-    //         } else if(data.at(2*k -1).at(j) == 'G' && data.at(2*k).at(j) == 'C') {
-    //             result[2*k]++;
-    //         } else if(data.at(2*k -1).at(j) == 'C' && data.at(2*k).at(j) == 'P') {
-    //             result[2*k-1]++;
-    //         } else if(data.at(2*k -1).at(j) == 'C' && data.at(2*k).at(j) == 'G') {
-    //             result[2*k]++;
-    //         } else if(data.at(2*k -1).at(j) == 'P' && data.at(2*k).at(j) == 'G') {
-    //             result[2*k-1]++;
-    //         } else if(data.at(2*k -1).at(j) == 'P' && data.at(2*k).at(j) == 'C') {
-    //             result[2*k]++;
-    //         }
-    //     }
-    // }
+    for(int j=0; j<m; j++) {
+        for(int k=0; k<n; k++) {
+            int player1 = result.at(2*k).second;
+            int player2 = result.at(2*k+1).second;
+            if(data.at(player1-1).at(j) == 'G' && data.at(player2-1).at(j) == 'C') {
+                result.at(2*k).first++;
+            } else if(data.at(player1-1).at(j) == 'G' && data.at(player2-1).at(j) == 'P') {
+                result.at(2*k+1).first++;
+            } else if(data.at(player1-1).at(j) == 'C' && data.at(player2-1).at(j) == 'G') {
+                result.at(2*k+1).first++;
+            } else if(data.at(player1-1).at(j) == 'C' && data.at(player2-1).at(j) == 'P') {
+                result.at(2*k).first++;
+            } else if(data.at(player1-1).at(j) == 'P' && data.at(player2-1).at(j) == 'G') {
+                result.at(2*k).first++;
+            } else if(data.at(player1-1).at(j) == 'P' && data.at(player2-1).at(j) == 'C') {
+                result.at(2*k+1).first++;
+            }
+        }
+        sort(result.begin(), result.end());
+        reverse(result.begin(), result.end());
+    }
 
-    // for(int i=0; i<2*n; i++) {
-    //     cout << result.at(i) << endl;
-    // }
+    for(int i=0; i<2*n-1; i++) {
+        if(result.at(i).first == result.at(i+1).first && result.at(i).second > result.at(i+1).second) {
+            sort(result.begin(), result.end());
+            reverse(result.begin(), result.end());
+        }
+    }
+
+    for(int i=0; i<2*n; i++) {
+        cout << result.at(i).first << " : " << result.at(i).second << endl;
+    }
 }
