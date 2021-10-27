ca6a404206e558c7cc6baa37d42fe8f6a67b70d6
takattty <r9r_celtics_t9t@icloud.com>
2021-10-27 11:53:24 +0900

    標準入力3問目が通らない

--- abs/ABC085C/main.cpp
+++ abs/ABC085C/main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
-    
+    long long n, y; cin >> n >> y;
+
+    int A = -1;
+    int B = -1;
+    int C = -1;
+
+    for(int i=0; i<=n; i++) {
+        for(int j=0; j+i<=n; j++) {
+            int k = n - i - j;
+            if((10000*i + 5000*j + 1000*k) == y)  {
+                A = i;
+                B = j;
+                C = k;
+                break;
+            }
+        }
+    }
+
+    cout << A << " " << B << " " << C << endl;
 }
